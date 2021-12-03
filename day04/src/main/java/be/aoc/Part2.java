56af9043d5924f05c4c92a8dadd3ab26f894c3e4
Kim Lauwers <kim.lauwers@acerta.be>
2021-12-03 14:49:53 +0100

    empty day 4

---
+++ day04/src/main/java/be/aoc/Part2.java
@@ -0,0 +1,17 @@
+package be.aoc;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.nio.file.Files.lines;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+public class Part2 {
+
+    public static void main(String[] args) throws Exception {
+        final Path path = Paths.get(Part2.class.getClassLoader().getResource("input.txt").toURI());
+        final List<String> lines = lines(path)
+                .collect(toUnmodifiableList());
+    }
+}
